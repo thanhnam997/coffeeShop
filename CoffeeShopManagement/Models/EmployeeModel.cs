1990de61fa6c9f3ed214cfbbbcf6c15085e5b135
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-11-14 20:53:53 -0600

    Delete CoffeeShopManagement directory

--- CoffeeShopManagement/Models/EmployeeModel.cs
+++
@@ -1,7 +0,0 @@
-public class Employee
-{
-    public int EmployeeId { get; set; }      // Primary Key
-    public string Name { get; set; }         // Employee Name
-    public string Role { get; set; }         // Job Role (e.g., Barista, Cashier)
-    public DateTime HireDate { get; set; }   // Date of Hire
-}
--- CoffeeShopManagement/Models/OrderDetailModel.cs
+++
@@ -1,9 +0,0 @@
-public class OrderDetail
-{
-    public int OrderDetailId { get; set; }       // Primary Key
-    public int OrderId { get; set; }             // Foreign Key to Order
-    public Order Order { get; set; }             // Navigation Property to Order
-    public int ProductId { get; set; }           // Foreign Key to Product
-    public Product Product { get; set; }         // Navigation Property to Product
-    public int Quantity { get; set; }            // Quantity Ordered
-}
